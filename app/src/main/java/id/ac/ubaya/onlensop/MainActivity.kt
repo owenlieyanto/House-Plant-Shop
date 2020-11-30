1b44e97431935697453193eece7f7756e038deed
Jericho AD <62791330+jerichoad@users.noreply.github.com>
2020-11-30 19:58:49 +0700

    perdamaian oh perdamaian '<'

--- app/src/main/java/id/ac/ubaya/onlensop/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/onlensop/MainActivity.kt
@@ -2,10 +2,46 @@ package id.ac.ubaya.onlensop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val fragments: ArrayList<Fragment> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        with(fragments) {
+            add(HomeFragment())
+            add(CartFragment())
+            add(HistoryFragment())
+            add(ProfileFragment())
+        }
+
+        viewPager.adapter = MyAdapter(this, fragments)
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                /*val menu = arrayOf(
+                    R.id.itemHome,
+                    R.id.itemCart,
+                    R.id.itemHistory,
+                    R.id.itemProfile
+                )
+                bottomNav.selectedItemId = menu[position]*/
+                bottomNav.selectedItemId = bottomNav.menu.getItem(position).itemId
+            }
+        })
+        bottomNav.setOnNavigationItemSelectedListener {
+            viewPager.currentItem = when(it.itemId){
+                R.id.itemHome -> 0
+                R.id.itemCart -> 1
+                R.id.itemHistory -> 2
+                R.id.itemProfile -> 3
+                else -> 0
+            }
+            true
+        }
+
     }
 }
---
+++ app/src/main/java/id/ac/ubaya/onlensop/MyAdapter.kt
@@ -0,0 +1,10 @@
+package id.ac.ubaya.onlensop
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MyAdapter(val activity: AppCompatActivity, val fragments: ArrayList<Fragment>): FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int = fragments.size
+    override fun createFragment(position: Int) = fragments[position]
+}
