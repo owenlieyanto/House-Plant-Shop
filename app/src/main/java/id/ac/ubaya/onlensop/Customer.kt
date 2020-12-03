031ba840fc68852ef0f5c18a7b3d28c344024955
owenlieyanto <62794634+owenlieyanto@users.noreply.github.com>
2020-12-03 18:26:27 +0700

    Penyempurnaan LoginActivity.kt
    Added Customer.kt; build.gradle.volley
    Update LoginActivity.kt support intent ke MainActivity

---
+++ app/src/main/java/id/ac/ubaya/onlensop/Customer.kt
@@ -0,0 +1,5 @@
+package id.ac.ubaya.onlensop
+
+import java.io.Serializable
+
+data class Customer(var email: String, var nama: String, var password: String, var wallet: Int):Serializable
--- app/src/main/java/id/ac/ubaya/onlensop/LoginActivity.kt
+++ app/src/main/java/id/ac/ubaya/onlensop/LoginActivity.kt
@@ -1,11 +1,21 @@
 package id.ac.ubaya.onlensop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 class LoginActivity : AppCompatActivity() {
+
+    val CUSTOMER = "customer"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -15,8 +25,41 @@ class LoginActivity : AppCompatActivity() {
             val password = textInputLoginPassword.text.toString()
 
             if (email != "" && password != "") {
-                Toast.makeText(this, "Email: $email\nPassword: $password", Toast.LENGTH_SHORT)
-                    .show()
+                val q = Volley.newRequestQueue(this)
+                val url = "http://ubaya.prototipe.net/nmp160418081/login.php"
+                var stringRequest = object : StringRequest(
+                    Request.Method.POST, url,
+                    {
+                        Log.d("apiresult", it)
+                        val obj = JSONObject(it)
+                        if (obj.getString("result") == "OK") {
+                            val data = obj.getJSONArray("data")
+
+                            var email = data.getJSONObject(0).getString("email")
+                            var nama = data.getJSONObject(0).getString("nama")
+                            var password = data.getJSONObject(0).getString("password")
+                            var wallet = data.getJSONObject(0).getString("wallet")
+
+                            val customer = Customer(email, nama, password, wallet.toInt())
+
+                            val intent = Intent(this, MainActivity::class.java)
+                            intent.putExtra(CUSTOMER, customer)
+                            startActivity(intent)
+                        }
+                    },
+                    {
+                        Log.e("apiresult", it.message.toString())
+                    }
+                ) {
+                    override fun getParams(): MutableMap<String, String> {
+                        val params = HashMap<String, String>()
+                        params["email"] = email
+                        params["password"] = password
+
+                        return params
+                    }
+                }
+                q.add(stringRequest)
             } else {
                 Toast.makeText(this, "Email dan Password wajib diisi.", Toast.LENGTH_SHORT).show()
             }
