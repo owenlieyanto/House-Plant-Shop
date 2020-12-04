54930bc83821bc3dd83ee910f9d32abf6f00ebbf
owenlieyanto <62794634+owenlieyanto@users.noreply.github.com>
2020-12-04 18:32:00 +0700

    Perbaikan activity_main.xml & product_card_layout.xml

--- app/src/main/java/id/ac/ubaya/onlensop/Product.kt
+++ app/src/main/java/id/ac/ubaya/onlensop/Product.kt
@@ -1,11 +1,13 @@
 package id.ac.ubaya.onlensop
+
 import java.io.Serializable
 
-data class Product(var id: Int,
-                   var name: String,
-                   var desc: String,
-                   var price: Int,
-                   var image: String,
-                   var stock: Int,
-                   var category: Int) {
-}
+data class Product(
+    var id: Int,
+    var name: String,
+    var desc: String,
+    var price: Int,
+    var image: String,
+    var stock: Int,
+    var category: Int
+)
